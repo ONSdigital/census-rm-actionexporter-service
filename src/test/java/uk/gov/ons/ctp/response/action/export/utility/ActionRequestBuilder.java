2203a8027721c459b5fdcf3e45d341cd434819c4
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-03-22 16:49:01 +0000

    remove old socialActionRequest

--- src/test/java/uk/gov/ons/ctp/response/action/export/utility/ActionRequestBuilder.java
+++ src/test/java/uk/gov/ons/ctp/response/action/export/utility/ActionRequestBuilder.java
@@ -65,40 +65,6 @@ public class ActionRequestBuilder {
     return actionRequest;
   }
 
-  public static ActionRequest createSocialActionRequest(
-      final String actionType, String addressLine1, String exerciseRef) {
-    ActionAddress actionAddress = new ActionAddress();
-    actionAddress.setLine1(addressLine1);
-    actionAddress.setCountry("E");
-    actionAddress.setOrganisationName("Castle of Frankenstein");
-    actionAddress.setPostcode("postCode");
-    actionAddress.setTownName("postTown");
-    actionAddress.setLocality("locality");
-
-    ActionRequest actionRequest = new ActionRequest();
-    actionRequest.setSampleUnitRef("sampleUR");
-    actionRequest.setActionId(UUID.randomUUID().toString());
-    actionRequest.setActionPlan("actionPlan");
-    actionRequest.setActionType(actionType);
-    actionRequest.setAddress(actionAddress);
-    actionRequest.setQuestionSet("questions");
-    actionRequest.setLegalBasis("legalBasis");
-    actionRequest.setRegion("region");
-    actionRequest.setRespondentStatus("rStatus");
-    actionRequest.setEnrolmentStatus("eStatus");
-    actionRequest.setCaseGroupStatus("cgStatus");
-    actionRequest.setCaseId(UUID.randomUUID().toString());
-    actionRequest.setPriority(Priority.HIGHEST);
-    actionRequest.setCaseRef("caseRef");
-    actionRequest.setIac("test-iac");
-    actionRequest.setExerciseRef(exerciseRef);
-    actionRequest.setContact(new ActionContact());
-    actionRequest.setEvents(new ActionEvent(Collections.singletonList("event1")));
-    actionRequest.setReturnByDate(DateTimeFormatter.ofPattern("dd/MM").format(LocalDate.now()));
-
-    return actionRequest;
-  }
-
   public static String actionInstructionToXmlString(ActionInstruction actionInstruction)
       throws JAXBException {
     JAXBContext jaxbContext = JAXBContext.newInstance(ActionInstruction.class);

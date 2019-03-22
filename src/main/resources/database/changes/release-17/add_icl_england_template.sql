01fedd26d9724d6867f31e46efa8df1b89cdd0e4
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-03-22 09:58:33 +0000

    1st stab at template for ICL_England

--- src/main/resources/database/changes/release-17/add_icl_england_template.sql
+++ src/main/resources/database/changes/release-17/add_icl_england_template.sql
@@ -1,17 +1,17 @@
-INSERT INTO actionexporter.template (templatenamepk, content, datemodified) VALUES ('socialReminder',
-'<#list actionRequests as actionRequest>
-${(actionRequest.address.line1?trim)!}:' ||
-'${(actionRequest.address.line2?trim)!}:' ||
-'${(actionRequest.address.postcode?trim)!}:' ||
-'${(actionRequest.address.townName?trim)!}:' ||
-'${(actionRequest.address.locality?trim)!}:' ||
-'${(actionRequest.address.country?trim)!}:' ||
-'${(actionRequest.iac?trim)!"null"}:' ||
-'${(actionRequest.address.organisationName?trim)!}:' ||
-'${(actionRequest.address.sampleUnitRef)!"null"}:' ||
-'${(actionRequest.returnByDate)!"null"}
+INSERT INTO actionexporter.template (templatenamepk, content, datemodified) VALUES ('iclEngland',
+'<#list actionRequests as actionRequest>' ||
+'${(actionRequest.iac?trim)!"null"}|' ||
+'ARID' ||
+'${(actionRequest.address.line1?trim)!}|' ||
+'${(actionRequest.address.line2?trim)!}|' ||
+'NOT YET IN MSG|' ||
+'${(actionRequest.address.townName?trim)!}|' ||
+'${(actionRequest.address.postcode?trim)!}|' ||
+'${(actionRequest.address.sampleUnitRef)!"null"}|' ||
+'ARID
 </#list>'
 ,
 now());
+
 
 UPDATE actionexporter.templatemapping SET templatenamefk = 'iclEngland' WHERE actiontypenamepk = 'ICL_ENGLAND';

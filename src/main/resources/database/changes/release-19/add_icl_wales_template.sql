86d862fa12eb0abdc7e1dc2a649e38f0476e93b8
Richard Weeks <richard.weeks@ons.gov.uk>
2019-04-24 09:50:54 +0100

    Add new print file template for Wales initial contact letter.

---
+++ src/main/resources/database/changes/release-19/add_icl_wales_template.sql
@@ -0,0 +1,21 @@
+INSERT INTO actionexporter.template
+(templatenamepk, content, datemodified)
+VALUES ('icl1e',
+'<#list actionRequests as actionRequest>' ||
+'${(actionRequest.iac?trim)!}|' ||
+'${(actionRequest.caseRef?trim)!}|' ||
+'${(actionRequest.address.line1?trim)!}|' ||
+'${(actionRequest.address.line2?trim)!}|' ||
+'${(actionRequest.address.line3?trim)!}|' ||
+'${(actionRequest.address.townName?trim)!}|' ||
+'${(actionRequest.address.postcode?trim)!}|' ||
+'P_IC_ICL2
+</#list>',
+now());
+
+
+INSERT INTO actionexporter.templatemapping
+(actiontypenamepk, templatenamefk, filenameprefix, datemodified)
+VALUES
+('ICL2E','icl2e', 'P_IC_ICL2', now());
+

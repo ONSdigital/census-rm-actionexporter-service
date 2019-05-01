9448d64da77c24dcbf1e7adf88315206207211d7
Richard Weeks <richard.weeks@ons.gov.uk>
2019-05-01 11:18:32 +0100

    Add DB changes for ICL Wales.

---
+++ src/main/resources/database/changes/release-19/add_icl_wales_template.sql
@@ -0,0 +1,20 @@
+INSERT INTO actionexporter.template
+(templatenamepk, content, datemodified)
+VALUES ('icl2e',
+'<#list actionRequests as actionRequest>' ||
+'${(actionRequest.iac?trim)!}|' ||
+'${(actionRequest.caseRef?trim)!}|' ||
+'${(actionRequest.address.line1?trim)!}|' ||
+'${(actionRequest.address.line2?trim)!}|' ||
+'${(actionRequest.address.line3?trim)!}|' ||
+'${(actionRequest.address.townName?trim)!}|' ||
+'${(actionRequest.address.postcode?trim)!}|' ||
+'P_IC_ICL2
+</#list>',
+now());
+
+
+INSERT INTO actionexporter.templatemapping
+(actiontypenamepk, templatenamefk, filenameprefix, datemodified, requesttype)
+VALUES
+('ICL2E','icl2e', 'P_IC_ICL2', now(), 'print_service');
